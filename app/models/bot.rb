9eeb1972986ee444a7afdbccd7fb6a564bf4695b
Pablo Ledesma <pablo.ledesma.p@gmail.com>
2015-10-12 15:43:28 -0400

    check twilio response in production

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -36,15 +36,20 @@ class Bot < ActiveRecord::Base
     )
   end
 
-  def self.route_incoming(params)
+  def route_incoming(params)
     message_sender = params[:From]
     # message_body = params[:Body]
     # user = User.find_by(phone_number: message_sender.sub("+1", ""))
     reply_body = UNKNOWN_COMMAND_MESSAGE
+    @twilio_number = ENV["TWILIO_PHONE_NUMBER"]
+    client = Twilio::REST::Client.new(
+      ENV["TWILIO_ACCOUNT_SID"],
+      ENV["TWILIO_AUTH_TOKEN"]
+    )
     client.account.messages.create(
       from: "#{@twilio_number}",
       to: user.full_phone_number,
-      body: "This is a test"
+      body: reply_body
     )
   end
 end
