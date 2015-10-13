2a7d83ba6651538714b73b197299b8ef35aa808e
Pablo Ledesma <pablo.ledesma.p@gmail.com>
2015-10-13 14:29:46 -0400

    twilio method from model

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -34,4 +34,23 @@ class Bot < ActiveRecord::Base
       body: body
     )
   end
+
+  def self.reply_body(params)
+    message_sender = params[:From]
+    message_body = params[:Body]
+
+    reply_body = "This is a test"
+    user = User.find_by(phone_number: message_sender.sub("+1", ""))
+
+    twilio_number = ENV["TWILIO_PHONE_NUMBER"]
+    client = Twilio::REST::Client.new(
+      ENV["TWILIO_ACCOUNT_SID"],
+      ENV["TWILIO_AUTH_TOKEN"]
+    )
+    client.account.messages.create(
+      from: "#{twilio_number}",
+      to: user.full_phone_number,
+      body: reply_body
+    )
+  end
 end
