1276bedc551f7d2795b66a7e4514516ead7380bb
Pablo Ledesma <pablo.ledesma.p@gmail.com>
2015-10-13 11:58:15 -0400

    conversation test2

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -6,11 +6,10 @@ class TwilioController < ApplicationController
   def answer
     message_sender = params[:From]
     message_body = params[:Body]
-    UNKNOWN_COMMAND_MESSAGE = "This is a test"
 
+    reply_body = "This is a test"
     user = User.find_by(phone_number: message_sender.sub("+1", ""))
 
-    reply_body = UNKNOWN_COMMAND_MESSAGE
     twilio_number = ENV["TWILIO_PHONE_NUMBER"]
     client = Twilio::REST::Client.new(
       ENV["TWILIO_ACCOUNT_SID"],
