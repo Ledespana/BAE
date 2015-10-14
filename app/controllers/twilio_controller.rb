fc41516f3034da516aa11c5c84548bb6bf198882
Pablo Ledesma <pablo.ledesma.p@gmail.com>
2015-10-14 15:23:54 -0400

    Indico Api implemented

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,7 +1,7 @@
 require "twilio-ruby"
 
 class TwilioController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   def answer
     Bot.reply_body(params)
