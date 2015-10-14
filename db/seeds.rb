c8cf7f505d45f914d638e42859d9162885a8c6bb
Pablo Ledesma <pablo.ledesma.p@gmail.com>
2015-10-14 14:11:26 -0400

    Indico Api implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,51 +1,49 @@
 User.create(
-    email: "pablo.ledesma.p@gmail.com",
-    username: "Pablo",
-    password: "923138334",
-    phone_number: Faker::Number.number(10),
-    gender: "Male"
-  )
+  email: "pablo.ledesma.p@gmail.com",
+  username: "Pablo",
+  password: "923138334",
+  phone_number: Faker::Number.number(10),
+  gender: "Male"
+)
 
 Bot.create(
-     name: "Louisa",
-     gender: "Female",
-     eye_color: "Green",
-     hair_color: "Blonde",
-     age: 26,
-     user_id: User.last.id
-   )
+  name: "Louisa",
+  gender: "Female",
+  eye_color: "Green",
+  hair_color: "Blonde",
+  age: 26,
+  user_id: User.last.id
+)
 #Sentences
 
 Interaction.create(
-       category: "Sentence",
-       sentence: "I love how you understand me",
-       response: "It's not easy but I have fun",
-       user_id: User.last.id
-     )
+  category: "Sentence",
+  sentence: "I love how you understand me",
+  response: "It's not easy but I have fun",
+  user_id: User.last.id
+)
 
 Interaction.create(
-      category: "Sentence",
-      sentence: "I promise",
-      response: "Great :)",
-      user_id: User.last.id
-    )
+  category: "Sentence",
+  sentence: "I promise",
+  response: "Great :)",
+  user_id: User.last.id
+)
 
 Interaction.create(
-       category: "Sentence",
-       sentence: "I love how you understand me",
-       response: "It's not easy but I have fun",
-       user_id: User.last.id
-     )
+  category: "Sentence",
+  sentence: "I love how you understand me",
+  response: "It's not easy but I have fun",
+  user_id: User.last.id
+)
 
 Interaction.create(
-       category: "Keyword",
-       sentiment: "Positive",
-       keyword1: "I want a new bike",
-       response: "You deserve it. Promise me you will think about getting one",
-       user_id: User.last.id
-
-     )
-
+  category: "Keyword",
+  sentiment: "Positive",
+  keyword1: "I want a new bike",
+  response: "You deserve it. Promise me you will think about getting one",
+  user_id: User.last.id
+)
 
 # 20.times do
 #   User.create(
