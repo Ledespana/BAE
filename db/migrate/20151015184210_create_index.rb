e7752e54df1acd59ff4dd8e702c2671d3732c910
Pablo Ledesma <pablo.ledesma.p@gmail.com>
2015-10-15 14:47:58 -0400

    Add index for interactions table

--- db/migrate/20151015184210_create_index.rb
+++ db/migrate/20151015184210_create_index.rb
@@ -1,5 +1,5 @@
 class CreateIndex < ActiveRecord::Migration
   def change
-    add_index :interactions, [:sentence, :keyword1, :keyword2], name: 'index_interactions'
+    add_index :interactions, [:sentence, :keyword1, :keyword2], name: "index_interactions"
   end
 end
