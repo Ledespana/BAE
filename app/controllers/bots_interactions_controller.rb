18f64febe2a11ea2ad042b205b175f33e8adef39
Pablo <pablo.ledesma.p@gmail.com>
2015-11-02 11:44:43 -0500

    Add JS add/delete interaction button

--- app/controllers/bots_interactions_controller.rb
+++ app/controllers/bots_interactions_controller.rb
@@ -2,12 +2,20 @@ class BotsInteractionsController < ApplicationController
   def create
     @bot = Bot.find(params[:bot_id])
     @bots_interaction = BotsInteraction.new(bots_interaction_params)
-    if @bots_interaction.save
-      flash[:notice] = "Interaction added to #{@bot.name}!"
-      redirect_to :back
-    else
-      flash[:errors] = "Something went wrong!"
-      redirect_to :back
+
+    respond_to do |f|
+      f.html {
+        if @bots_interaction.save
+          flash[:notice] = "Interaction added to #{@bot.name}!"
+           redirect_to :back
+        else
+          flash[:errors] = "Something went wrong!"
+           redirect_to :back
+        end
+      }
+      f.js {
+        @bots_interaction.save
+      }
     end
   end
 
@@ -18,13 +26,21 @@ class BotsInteractionsController < ApplicationController
       bot_id: @bot.id,
       interaction_id: @interaction.id
     )
-    if current_user
-      @bots_interaction[0].destroy
-      flash[:notice] = "Interaction removed successfully from #{@bot.name}"
-      redirect_to :back
-    else
-      flash[:error] = "You have no permission to modify that"
-      redirect_to :back
+    respond_to do |f|
+      f.html {
+        if current_user
+          @bots_interaction[0].destroy
+          flash[:notice] = "Interaction removed successfully from #{@bot.name}"
+          redirect_to :back
+        else
+          flash[:error] = "You have no permission to modify that"
+          redirect_to :back
+        end
+      }
+
+      f.js {
+        @bots_interaction[0].destroy
+      }
     end
   end
 
