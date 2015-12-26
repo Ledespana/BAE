c74c4815026efff0a57150b856a7d14c84dd81b5
Pablo <pablo.ledesma.p@gmail.com>
2015-12-26 13:22:40 -0500

    Fix pagination.final

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,15 @@ User.create(
   phone_number: Faker::Number.number(10),
   gender: "Male",
 )
+12.times do
+  User.create(
+    email: Faker::Internet.email,
+    username: Faker::Name.first_name,
+    password: Faker::Internet.password(8),
+    phone_number: Faker::Number.number(10),
+    gender: "Male",
+    )
+end
 
 Bot.create(
   name: "Louisa",
